4d30dc09206276741b97fb73fd05628ae5812ec2
Maurisss94 <maurobale94@hotmail.es>
2015-05-10 19:46:25 +0200

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require("http").Server(app);  //necessitem http per treballar amb socket.io
-
+var port = process.env.PORT || 8080;
 app.use('/',require("./controllers/static"));
 
 require("./controllers/xat")(http);
 
 
-http.listen(8000, process.env.IP,function() {
-    console.log('listening on host '+process.env.IP+':'+process.env.PORT);
+http.listen(port, process.env.IP,function() {
+    console.log('listening on host '+process.env.IP+':'+port);
 });
